c1d925231f37c04888e4a90a1fcee5fe32b25ff8
kalechao <kalechao@outlook.com>
2016-07-18 14:45:36 +0800

    优化测试
    优化测试

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+/**
+  Author: Kale Chao | FakeCityMan
+  Blog: http://kalechao87.github.io/
+**/
+(function($) {
+    $(document).ready(function() {
+        console.log('Ready');
+
+    });  //Document ready
+})(jQuery);
